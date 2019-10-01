7b471b73fd5e8e28d40b94708f814fe960bc512c
GabePetkewich <38142306+GabePetkewich@users.noreply.github.com>
2019-09-30 20:22:49 -0400

    prototype

---
+++ Assets/walk.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walk : MonoBehaviour {
+
+	public float m_speed = 5;
+	public float r_speed = 5;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.forward * Input.GetAxis ("Vertical") * Time.deltaTime * m_speed, Space.Self);
+		transform.Rotate (0, Input.GetAxis ("Horizontal") * Time.deltaTime * r_speed, 0);
+	}
+}
