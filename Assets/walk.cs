fac1b46f70817237351dafe5f2f0b68aa0818bd8
brian <bc06255@georgiasouthern.edu>
2019-10-16 17:48:09 -0400

    Added jump & removed random moving after hitting a wall

--- Assets/walk.cs
+++ Assets/walk.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 
 public class walk : MonoBehaviour
 {
-
+    Rigidbody rb;
     public float speed = 5f;
     public float maxSpeed = 15f;
+    public Vector3 vel;
+    public float canJump = 0f;
     // Use this for initialization
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        vel = rb.velocity;
+        rb.angularVelocity = Vector3.zero;
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > canJump)
+        {
+            rb.AddForce(new Vector3(0, 250, 0), ForceMode.Impulse);
+            canJump = Time.time + 0.7f;
+        }
+
         if (Input.GetKey(KeyCode.LeftShift) && speed < maxSpeed)
         {
             speed = speed + 10f * Time.deltaTime;
@@ -46,5 +57,13 @@ public class walk : MonoBehaviour
             transform.Translate(Vector3.right * Time.deltaTime * speed);
         }
 
+        //forces player to stop moving if no buttons are pressed
+        //else
+        //{
+        //    rb.angularVelocity = Vector3.zero;
+        //}
+
     }
+
+
 }
