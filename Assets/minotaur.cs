044c33c83bac7baf8d344c54cfc6be4c2c491ea2
GabePetkewich <38142306+GabePetkewich@users.noreply.github.com>
2019-11-22 14:56:32 -0500

    nav mesh and pic puzzle no script

---
+++ Assets/minotaur.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class minotaur : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform goal;
+    public GameObject player;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        goal = player.transform;
+        agent.SetDestination(goal.position);
+    }
+}
