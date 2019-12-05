1dba86e5539153d55e2c45b085b30184e937c03e
Carson <acarsondowns@gmail.com>
2019-12-05 00:21:32 -0500

    few changes to tile puzzle. mostly a test commit

---
+++ Assets/Carson's Shit/Jumping Section/TileTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileTrigger : MonoBehaviour
+{
+    public GameObject tile;
+    [SerializeField]
+    GameObject collider;
+    [SerializeField]
+    bool isDeath;
+    Shader shader1;
+
+    void OnEnable() 
+    {
+        collider = this.gameObject;
+        tile = this.transform.parent.gameObject;
+        shader1 = Shader.Find("UI/Unlit/Transparent");
+        collider.GetComponent<MeshRenderer>().material.shader = shader1;
+        collider.GetComponent<MeshRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 0f);
+        if(!isDeath)
+        {
+            collider.SetActive(false);
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger entered");
+        if(isDeath == true)
+            tile.GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Trigger exited");
+    }
+}
--- Assets/Scripts/TileTester.cs
+++ Assets/Scripts/TileTester.cs
@@ -4,16 +4,12 @@ using UnityEngine;
 
 public class TileTester : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    Shader shader1;
+
+    void OnEnable()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        shader1 = Shader.Find("UI/Unlit/Transparent")
     }
 
     void OnTriggerEnter(Collider collider)
@@ -21,7 +17,8 @@ public class TileTester : MonoBehaviour
         switch (collider.gameObject.tag)
         {
             case "Incorrect":
-                collider.GetComponent<MeshRenderer>().material.color = Color.red;
+                collider.GetComponent<MeshRenderer>().material.shader = shader1;
+                collider.GetComponent<MeshRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 0f);
                 gameObject.transform.position = new Vector3(34.24f, 1.87f, 9.08f);
                 break;
         }
